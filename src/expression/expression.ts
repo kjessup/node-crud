41d2d799754355d4686b5d7df1bc8745c519d0b6
Kyle Jessup <kyle.jessup@treefrog.ca>
2023-09-18 16:16:32 -0400

    save

--- src/expression/expression.ts
+++ src/expression/expression.ts
@@ -1,77 +1,130 @@
+import { CRUDSQLGenError, SQLGenState } from "..";
 
 export type ExpressionProducer = () => CRUDExpression;
 
-export abstract class CRUDExpression {}
+export abstract class CRUDExpression {
+    sqlSnippet(state: SQLGenState): string {
+        throw new CRUDSQLGenError('sqlSnippet not implemented.');
+    }
+    primitiveType(): any | undefined {
+        return undefined;
+    }
+}
+
+export abstract class CRUDBooleanExpression {
+    
+}
 
 export class ColumnExpression extends CRUDExpression {
     constructor(public column: string) {
         super();
     }
-}
-
-export class AndExpression extends CRUDExpression {
-    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
-        super();
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.quote(this.column);
     }
 }
 
-export class OrExpression extends CRUDExpression {
+export class AndExpression extends CRUDBooleanExpression {
     constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
         super();
     }
-}
-
-export class EqualityExpression extends CRUDExpression {
-    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
-        super();
+    sqlSnippet(state: SQLGenState): string {
+        return `(${this.lhs.sqlSnippet(state)} AND ${this.rhs.sqlSnippet(state)})`;
     }
 }
 
-export class InEqualityExpression extends CRUDExpression {
+export class OrExpression extends CRUDBooleanExpression {
     constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return `(${this.lhs.sqlSnippet(state)} OR ${this.rhs.sqlSnippet(state)})`;
+    }
 }
 
-export class NotExpression extends CRUDExpression {
+export class EqualityExpression extends CRUDBooleanExpression {
+    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
+        super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} = ${this.rhs.sqlSnippet(state)}`;
+    }
+}
+
+export class InEqualityExpression extends CRUDBooleanExpression {
+    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
+        super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} <> ${this.rhs.sqlSnippet(state)}`;
+    }
+}
+
+export class NotExpression extends CRUDBooleanExpression {
     constructor(public rhs: CRUDExpression) {
         super();
     }
-}
-
-export class LessThanExpression extends CRUDExpression {
-    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
-        super();
+    sqlSnippet(state: SQLGenState): string {
+        return `NOT ${this.rhs.sqlSnippet(state)}`;
     }
 }
 
-export class LessThanEqualExpression extends CRUDExpression {
+export class LessThanExpression extends CRUDBooleanExpression {
     constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
         super();
     }
-}
-
-export class GreaterThanExpression extends CRUDExpression {
-    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
-        super();
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} < ${this.rhs.sqlSnippet(state)}`;
     }
 }
 
-export class GreaterThanEqualExpression extends CRUDExpression {
+export class LessThanEqualExpression extends CRUDBooleanExpression {
     constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} <= ${this.rhs.sqlSnippet(state)}`;
+    }
 }
 
-export class InExpression extends CRUDExpression {
+export class GreaterThanExpression extends CRUDBooleanExpression {
+    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
+        super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} > ${this.rhs.sqlSnippet(state)}`;
+    }
+}
+
+export class GreaterThanEqualExpression extends CRUDBooleanExpression {
+    constructor(public lhs: CRUDExpression, public rhs: CRUDExpression) {
+        super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} >= ${this.rhs.sqlSnippet(state)}`;
+    }
+}
+
+export class InExpression extends CRUDBooleanExpression {
     constructor(public lhs: CRUDExpression, public rhs: CRUDExpression[]) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return `${this.lhs.sqlSnippet(state)} IN (${this.rhs.map(e => e.sqlSnippet(state))})`;
+    }
 }
 
-export class LikeExpression extends CRUDExpression {
-    constructor(public lhs: CRUDExpression, public wild1: boolean, public string: string, public wild2: boolean) {
+export class LikeExpression extends CRUDBooleanExpression {
+    constructor(
+        public lhs: CRUDExpression, 
+        public wild1: boolean, 
+        public string: string, 
+        public wild2: boolean) {
         super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        const lstr = `${this.wild1 ? '%' : ''}${this.string.replace(/%/g, '\\%')}${this.wild2 ? '%' : ''}`;
+        return `${this.lhs.sqlSnippet(state)} LIKE ${new StringExpression(lstr).sqlSnippet(state)})`;
     }
 }
 
@@ -79,11 +132,20 @@ export class LazyExpression extends CRUDExpression {
     constructor(public expressionProducer: ExpressionProducer) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return this.expressionProducer().sqlSnippet(state);
+    }
 }
 
 export class IntegerExpression extends CRUDExpression {
     constructor(public int: number) {
         super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.int;
     }
 }
 
@@ -91,11 +153,23 @@ export class DecimalExpression extends CRUDExpression {
     constructor(public decimal: number) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.decimal;
+    }
 }
 
 export class StringExpression extends CRUDExpression {
     constructor(public str: string) {
         super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.str;
     }
 }
 
@@ -103,11 +177,23 @@ class BlobExpression extends CRUDExpression {
     constructor(public blob: Uint8Array) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.blob;
+    }
 }
 
 class SBlobExpression extends CRUDExpression {
     constructor(public sblob: Int8Array) {
         super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.sblob;
     }
 }
 
@@ -116,11 +202,23 @@ export class BoolExpression extends CRUDExpression {
     constructor(public bool: boolean) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.bool;
+    }
 }
 
 export class DateExpression extends CRUDExpression {
     constructor(public date: Date) {
         super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.date;
     }
 }
 
@@ -128,10 +226,19 @@ export class UUIDExpression extends CRUDExpression {
     constructor(public uuid: string) {
         super();
     }
+    sqlSnippet(state: SQLGenState): string {
+        return state.delegate.getBinding(this);
+    }
+    primitiveType(): any | undefined {
+        return this.uuid;
+    }
 }
 
 export class NullExpression extends CRUDExpression {
     constructor() {
         super();
+    }
+    sqlSnippet(state: SQLGenState): string {
+        return 'NULL';
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,427 @@
 
-import { CRUDExpression } from "./expression/expression";
-import { IDatabaseConnection } from "./database";
-// import { Table } from "./table";
-// import { Join } from "./join";
-// import { Select, Ordering, Limit } from "./select";
-// import { Where } from "./where";
+import { CRUDBooleanExpression, CRUDExpression } from "./expression/expression";
+
+export class CRUDSQLGenError extends Error {}
+export class CRUDSQLExeError extends Error {}
 
 export type Expression = CRUDExpression;
 export type ExpressionBinding = [string, Expression];
-export type Bindings = ExpressionBinding[];
+export type ObjectBinding = [string, any];
+export type Bindings = ObjectBinding[];
+
+export interface IDatabaseConnection {
+    sqlGenDelegate: SQLGenDelegate;
+    sqlExeDelegate(forSQL: string): SQLExeDelegate;
+}
 
 export type GConstructor<T = {}> = new (...args: any[]) => T;
 
 export class CRUDObjectBase {
-    constructor(public databaseConnection: IDatabaseConnection) {}
+    constructor(public databaseConnection: IDatabaseConnection) { }
+    setState(state: SQLGenState) { }
+    setSQL(state: SQLGenState) { }
 }
 
-export class CRUDCommandBase extends CRUDObjectBase {
-    constructor(databaseConnection: IDatabaseConnection) {
-        super(databaseConnection);
+export class CRUDFromObjectBase extends CRUDObjectBase {
+    constructor(public from: CRUDObjectBase) {
+        super(from.databaseConnection);
+    }
+    setState(state: SQLGenState): void {
+        this.from.setState(state);
+    }
+    setSQL(state: SQLGenState): void {
+        this.from.setSQL(state);
     }
 }
 
-export interface IModelObject {
-    
+export class CRUDCommandBase extends CRUDFromObjectBase {
+    sqlGenState?: SQLGenState;
+    constructor(from: CRUDObjectBase) {
+        super(from);
+    }
 }
 
-export interface TableDesc<O extends IModelObject> {
-    table: string;
-    pk: string;
+export interface SQLExeDelegate { 
+    exe<Shape extends Object>(bindings: ExpressionBinding[]): Promise<Shape[]>;
 }
 
-export interface FieldDesc {
-    tableAlias: string;
-    fieldName: string;
-    alias?: string;
-    function?: string;
-    distinct?: boolean;
+export interface SQLGenDelegate { 
+    bindings: ExpressionBinding[];
+	getBinding(forExpr: Expression): string;
+    quote(identifier: string): string;
 }
 
-export interface SubQueryDesc {
-    subQuery: QueryExpression;
+export enum SQLCommand {
+    select, insert, update, delete, count, unknown
+}
+
+export type SQLStatement = {
+    sql: string;
+    bindings: ExpressionBinding[];
+}
+
+export type SQLTableData = {
+    tableName: string;
     alias: string;
+    joinData?: SQLJoinData;
 }
 
-export type SelectedField = FieldDesc | SubQueryDesc;
-
-export type Selectable = SelectedField[];
-
-export interface InsertObject {
-    columns?: string[]; // An array that represents the specific columns we want to insert into.
-    values: any[]; // The values we want to insert into the table (rows of data).
+export type SQLJoinData = {
+    sourceColumn: string,
+    joinTable: string,
+    joinColumn: string
 }
 
-export interface UpdateObject {
-    set: Record<string, any>; // A collection of column and value pairs to update.
+export type SQLOrdering = {
+    by: string;
+    desc: boolean;
 }
 
-export interface JoinDesc {
-    sourceTableAlias: string;
-    sourceKeyColumn: string;
-    joinTableAlias: string;
-    joinKeyColumn: string;
-    joinSelectColumns: FieldDesc[];
+export type SQLLimit = {
+    max: number, 
+    skip: number
 }
 
-export interface QueryExpression extends CRUDExpression {
-    tables?: Record<string, TableDesc<IModelObject>>;
-    select?: Selectable;
-    where?: Expression;
-    delete?: Expression;
-    insert?: InsertObject;
-    update?: UpdateObject;
-    join?: JoinDesc[];
+export class SQLGenState {
+    constructor(public delegate: SQLGenDelegate) { }
+    command = SQLCommand.unknown;
+    tableData: SQLTableData[] = [];
+    whereExpr?: CRUDBooleanExpression;
+    accumulatedOrderings: SQLOrdering[] = [];
+    currentLimit?: SQLLimit;
+    statement: SQLStatement = {sql:'', bindings:[]};
+
+    private aliasCounter = 0;
+
+    addTable(tableName: string, joinData: SQLJoinData | undefined = undefined) {
+        this.tableData.push(
+            {tableName, alias: this.nextAlias(), joinData})
+    }
+
+    private nextAlias(): string {
+        return `t${this.aliasCounter++}`;
+    }
+};
+
+type MasterTable = {
+    table: SQLTableData;
+    delegate: SQLExeDelegate;
 }
 
+function zip<A, B>(a: A[], b: B[]): [A, B][] {
+    const length = Math.min(a.length, b.length);
+    const zipped: [A, B][] = [];
+    for(let i = 0; i < length; i++) {
+        zipped.push([a[i], b[i]]);
+    }
+    return zipped;
+}
+
+export class SQLTopExeDelegate implements SQLExeDelegate {
+    master: MasterTable;
+
+    constructor(public state: SQLGenState, database: IDatabaseConnection) {
+        this.master = {table: state.tableData[0], delegate: database.sqlExeDelegate(state.statement.sql)};
+    }
+    exe<Shape extends Object>(bindings: ExpressionBinding[]): Promise<Shape[]> {
+        return this.master?.delegate.exe(bindings);
+    }    
+}
+
+export class Database {
+    constructor(public databaseConnection: IDatabaseConnection) {}
+    table(name: string): Table {
+        return new Table(this.databaseConnection, name);
+    }
+    async run(statement: string, bindings: ExpressionBinding[]): Promise<void> {
+        const delegate = this.databaseConnection.sqlExeDelegate(statement);
+		await delegate.exe<{}>(bindings);
+        return;
+    }
+    async sql<Shape extends Object>(statement: string, bindings: ExpressionBinding[]): Promise<Shape[]> {
+        let ret: Shape[] = [];
+        const delegate = this.databaseConnection.sqlExeDelegate(statement);
+		return await delegate.exe<Shape>(bindings);
+    }
+}
+
+export class Table extends 
+        SelectableMixin(
+        WhereableMixin(
+        JoinableMixin(
+        OrderableMixin(
+        UpdateableMixin(
+        DeleteableMixin(
+        LimitableMixin(
+        InsertableMixin(
+            CRUDObjectBase)))))))) {
+    constructor(databaseConnection: IDatabaseConnection, public tableName: string) {
+        super(databaseConnection);
+    }
+    setState(state: SQLGenState): void {
+        state.addTable(this.tableName);
+    }
+    setSQL(state: SQLGenState): void {
+        const { delegate, accumulatedOrderings: orderings, currentLimit: limit } = state;
+        const t0 = state.tableData[0];
+        const tx = state.tableData.splice(1);
+
+        const aliasMap: any = state.tableData.reduce((obj, item) => {
+            return { 
+              ...obj, 
+              [item.tableName]: item.alias
+            };
+          }, {});
+
+        const nameQ = delegate.quote(t0.tableName);
+        const aliasQ = delegate.quote(t0.alias);
+
+        switch (state.command) {
+            case SQLCommand.select, SQLCommand.count: {
+                const sqlStr = 
+                    `SELECT ${state.tableData.map(t => `${t.alias}.*`).join(',')} 
+                    FROM ${nameQ} AS ${aliasQ}`;
+                const joinStr = 
+                    [' ', ...tx.map(t => 
+                    `JOIN ${t.tableName} AS ${t.alias} 
+                        ON ${t.alias}.${t.joinData?.sourceColumn} = ${aliasMap[t.joinData!.joinTable]}.${t.joinData?.joinColumn}`)].join('\n');
+                if (state.whereExpr != undefined) {
+
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
+
+export interface Selectable {
+    select<Shape extends Object>(): Select<Shape>;
+    first<Shape extends Object>(): Promise<Shape | undefined>;
+    count(): Promise<number>;
+}
+
+export function SelectableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Selectable {
+        select<Shape extends Object>(): Select<Shape> {
+            return new Select(this);
+        }
+        async first<Shape extends Object>(): Promise<Shape | undefined> {
+            const rows = await this.select<Shape>().rows();
+            for (let row of rows) {
+                return row;
+            }
+            return undefined;
+        }
+        async count(): Promise<number> {
+            let state = new SQLGenState(this.databaseConnection.sqlGenDelegate);
+            state.command = SQLCommand.count;
+            this.setState(state);
+            this.setSQL(state);
+            
+            const stat = state.statement.sql;
+		    const exeDelegate = this.databaseConnection.sqlExeDelegate(stat);
+            const results = await exeDelegate.exe<{count:number}>([]);
+            return results[0].count;
+        }
+    };
+}
+
+export class Select<Shape extends Object> extends CRUDCommandBase {
+    constructor(from: CRUDObjectBase) {
+        super(from);
+        let state = new SQLGenState(this.databaseConnection.sqlGenDelegate);
+        state.command = SQLCommand.select;
+        this.setState(state);
+        this.setSQL(state);
+        if (state.accumulatedOrderings.length != 0) {
+            throw new CRUDSQLGenError(`Orderings were not consumed: ${state.accumulatedOrderings}`)
+        }
+        this.sqlGenState = state;
+    }
+    async rows(): Promise<Shape[]> {
+        const gen = new SQLTopExeDelegate(this.sqlGenState!, this.databaseConnection);
+        return await gen.exe<Shape>(this.sqlGenState?.delegate.bindings ?? []);
+    }
+}
+
+export enum OrderDirection {
+    ascending, descending
+}
+
+export interface Orderable {
+    order(by: string, direction: OrderDirection): Ordering;
+}
+
+export function OrderableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Orderable {
+        order(by: string, direction: OrderDirection): Ordering {
+            return new Ordering(this, by, direction);
+        }
+    };
+}
+
+export class Ordering extends 
+        OrderableMixin(
+        JoinableMixin(
+        SelectableMixin(
+        WhereableMixin(
+        LimitableMixin(
+            CRUDFromObjectBase))))) {
+    constructor(from: CRUDObjectBase, public by: string, public direction: OrderDirection) {
+        super(from);
+    }
+    setSQL(state: SQLGenState): void {
+        state.accumulatedOrderings.push(
+            {by: this.by, desc: this.direction == OrderDirection.descending});
+        super.setSQL(state);
+    }
+}
+
+export interface Limitable {
+    limit(max: number, skip: number): Limit;
+}
+
+export function LimitableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Limitable {
+        limit(max: number, skip: number): Limit {
+            return new Limit(this, max, skip);
+        }
+    };
+}
+
+export class Limit extends 
+        OrderableMixin(
+        JoinableMixin(
+        SelectableMixin(
+        WhereableMixin(
+            CRUDFromObjectBase)))) {
+    constructor(from: CRUDObjectBase, public max: number, public skip: number) {
+        super(from);
+    }
+    setSQL(state: SQLGenState): void {
+        state.currentLimit = {max: this.max, skip: this.skip};
+        super.setSQL(state);
+    }
+}
+
+export interface Joinable {
+    join(srcTable: string, srcCol: string, joinTable: string, joinCol: string): Join;
+}
+
+export function JoinableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Joinable {
+        join(sourceTable: string, sourceCol: string, joinTable: string, joinCol: string): Join {
+            return new Join(this, sourceTable, sourceCol, joinTable, joinCol);
+        }
+    };
+}
+
+export class Join extends 
+        WhereableMixin(
+        SelectableMixin(
+        JoinableMixin(
+        OrderableMixin(
+        LimitableMixin(
+            CRUDFromObjectBase))))) {
+    constructor(from: CRUDObjectBase, 
+        public sourceTable: string,
+        public sourceColumn: string,
+        public joinTable: string,
+        public joinColumn: string) {
+        super(from);
+    }
+    setState(state: SQLGenState): void {
+        super.setState(state);
+        state.addTable(this.sourceTable, {
+            sourceColumn: this.sourceColumn,
+            joinTable: this.joinTable,
+            joinColumn: this.joinColumn});
+    }
+}
+
+export interface Whereable {
+    where(expr: CRUDExpression): Where;
+}
+
+export function WhereableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Whereable {
+        where(expr: CRUDBooleanExpression): Where {
+            return new Where(this, expr);
+        }
+    };
+}
+
+export class Where extends SelectableMixin(CRUDFromObjectBase) {
+    constructor(from: CRUDObjectBase, public expr: CRUDBooleanExpression) {
+        super(from);
+    }
+    setState(state: SQLGenState): void {
+        super.setState(state);
+        state.whereExpr = this.expr;
+    }
+}
+
+export interface Updateable {
+    update(): Update;
+}
+
+export function UpdateableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Updateable {
+        update(): Update {
+            return new Update(this);
+        }
+    };
+}
+
+export class Update extends CRUDCommandBase {
+    constructor(from: CRUDObjectBase) {
+            super(from);
+        }
+}
+
+export interface Deleteable {
+    delete(): Delete;
+}
+
+export function DeleteableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Deleteable {
+        delete(): Delete {
+            return new Delete(this);
+        }
+    };
+}
+
+export class Delete extends CRUDCommandBase {
+    constructor(from: CRUDObjectBase) {
+        super(from);
+        const delegate = this.databaseConnection.sqlGenDelegate;
+        let state = new SQLGenState(delegate);
+        state.command = SQLCommand.delete;
+        this.setState(state);
+        this.setSQL(state);
+        this.sqlGenState = state;
+        const stat = state.statement
+        let exeDelegate = this.databaseConnection.sqlExeDelegate(stat.sql);
+        exeDelegate.exe<{}>(state.delegate.bindings);
+    }
+}
+
+export interface Insertable {
+    insert(): Insert;
+}
+
+export function InsertableMixin<T extends GConstructor<CRUDObjectBase>>(Base: T) {
+    return class extends Base implements Insertable {
+        insert(): Insert {
+            return new Insert(this);
+        }
+    };
+}
+
+export class Insert extends CRUDCommandBase {
+    constructor(from: CRUDObjectBase) {
+            super(from);
+        }
+}
